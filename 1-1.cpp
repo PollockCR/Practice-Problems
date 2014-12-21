565aeee51c9c1bffc2275cb54b360a07e9b1cae0
Catherine Pollock <pollockcr@gmail.com>
2014-12-20 18:26:25 -0800

    Given solution for 1-1
    Left out more optimized solution because it requires information not given about problem.

--- 1-1.cpp
+++ 1-1.cpp
@@ -1,19 +1,19 @@
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+// no extra data structures implementation
+// time complexity O( n ) where n is length of string (Linear/Brute Force). Space complexity O( 1 ).
 bool allUniqueChars( String s ) const {
+  if( s.length() > 256 ){
+    return false;
+  }
+  bool charSet[ 256 ];
   for( int i = 0; i < s.size(); i++ ){
-    if( s.rfind( s[ i ] ) != i ){
+    if( charSet[ s[i] ] ){
       return false;
     }
+    charSet[ s[i] ] = true;
   }
   return true;
 }
 
-bool allUniqueChars( string s ) const {
-  list<char> sList;
-  int startSize;
-  sList.assign( s.begin(), s.end() );
-  startSize = sList.size();
-  sList.sort();
-  sList.unique;
-  return( sList.size() == startSize );
-}
+// if done by checking each character to every other character, time complexity O( n^2 ) and O(1) space 
